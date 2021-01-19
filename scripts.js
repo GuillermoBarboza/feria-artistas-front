b2b7cca09f435bf878d712ccd2c7faa38be19cd8
Guishe <gui.bar.jim@gmail.com>
2021-01-18 22:45:10 -0300

    now changes stickers stored on aws bucket

--- scripts.js
+++ scripts.js
@@ -1,16 +1,21 @@
+const flyers = [
+  "https://carlitosbucket.s3-sa-east-1.amazonaws.com/tero-rinonera.jpeg",
+  "https://carlitosbucket.s3-sa-east-1.amazonaws.com/capincho-mate.jpeg",
+  "https://carlitosbucket.s3-sa-east-1.amazonaws.com/capibara-purse.jpeg",
+  "https://carlitosbucket.s3-sa-east-1.amazonaws.com/niandu-playa.jpeg",
+  "https://carlitosbucket.s3-sa-east-1.amazonaws.com/niandu-trap.jpeg",
+  "https://carlitosbucket.s3-sa-east-1.amazonaws.com/tero-acuarela.jpeg",
+  "https://carlitosbucket.s3-sa-east-1.amazonaws.com/tero-remera.jpeg",
+];
+
 window.onload = () => {
-    let button = document.querySelector('button')
-    let div = document.querySelector('#apiDiv')
-    button.addEventListener('click', e => {
-        fetch('https://feria-de-artistas-back.vercel.app/')
-        .then(response => {
-            console.log(response)
-            return response.json()
-        })
-        .then(data => {
-            let p = document.createElement('p')
-            p.innerHTML = data
-            div.appendChild(p)
-        })
-    })
-}
+  let button = document.querySelector("button");
+  let body = document.querySelector("body");
+  let imgIndex = 0;
+  button.addEventListener("click", (e) => {
+    console.log(imgIndex)
+    imgIndex === 6 ? (imgIndex = 0) : imgIndex++;
+    console.log(imgIndex)
+    body.style.backgroundImage = `url(${flyers[imgIndex]})`;
+  });
+};
